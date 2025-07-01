07eae45cc612ec5d3169ce05266745c13ea00686
omar reda <omaraladeeb45@gmail.com>
2025-07-01 18:53:30 +0300

    update in add listing

--- src/app/environments/environment.ts
+++ src/app/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   apiurlauth: `https://localhost:7188/api/Account`,
-  apiurllisting: `https://localhost:7188/api/listings`,
+  apiurllisting: `https://localhost:7188/api/listing`,
   googleAuthUrl: 'https://localhost:7188/api/Account/externallogin/google',
   googleCallbackUrl: 'https://localhost:7188/signin-google',
 };
